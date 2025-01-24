e0df61fe0f5e9e5bce45b680529f88a1a7e441f1
Sakshi Antala <sakshi@saasinnova.com>
2025-01-24 15:24:01 +0530

    add capitalize-first class

--- CommonTooltip.tsx
+++ CommonTooltip.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Tooltip } from 'primereact/tooltip';
-import { ICommonTooltip } from './commonTooltip.model';
+import React from "react";
+import { Tooltip } from "primereact/tooltip";
+import { ICommonTooltip } from "./commonTooltip.model";
 
 const CommonTooltip = (props: ICommonTooltip) => {
-    const { attribute, tooltipContent, position } = props;
-    const elementRef = React.useRef<any>(null);
+  const { attribute, tooltipContent, position } = props;
+  const elementRef = React.useRef<any>(null);
 
-    return (
-        <React.Fragment>
-            <label htmlFor={attribute}>{attribute}</label> {''}
-            (<i ref={elementRef} className="pi pi-info text-black-500" />)
-            <Tooltip className="w-25rem" target={elementRef?.current} content={tooltipContent} position={position ?? 'top'} />
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <label htmlFor={attribute}>{attribute}</label> {""}
+      (<i ref={elementRef} className="pi pi-info text-black-500" />)
+      <Tooltip
+        className="w-25rem capitalize-first"
+        target={elementRef?.current}
+        content={tooltipContent}
+        position={position ?? "top"}
+      />
+    </React.Fragment>
+  );
 };
 
 export default CommonTooltip;
